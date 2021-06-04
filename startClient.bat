1fa33131b5d621f4a67de46db4c6ceded8cee48c
Xanz <xanrealmz@hotmail.co.uk>
2021-06-04 18:25:06 +0100

    path fixes

--- startClient.bat
+++ startClient.bat
@@ -1,1 +1,1 @@
-start Quake4.exe  +set com_allowConsole 1 +set logfile 2 +set fs_game xanzcoop +set win_allowmultipleInstances 1 +set fs_savepath "G:\SteamLibrary\steamapps\common\Quake 4\client\" +connect localhost
+start Quake4.exe  +set com_allowConsole 1 +set logfile 2 +set fs_game xanzcoop +set win_allowmultipleInstances 1 +set fs_savepath "C:\Program Files (x86)\Steam\steamapps\common\Quake 4\client\" +connect localhost
