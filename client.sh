57bfdb5e9c32f38b1ec9c3f4e2cc58c0ebfc57d6
Xanz <xanrealmz@hotmail.co.uk>
2022-07-18 22:18:13 +0100

    Add linux start scripts

---
+++ client.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+quake4 +set fs_game xanzcoop +set si_pure 0 +set win_allowmultipleInstances 1 +set developer 1 +connect localhost +set fs_savepath "./client"
---
+++ host.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+quake4 +set fs_game xanzcoop +set si_pure 0 +set win_allowmultipleInstances 1 +set developer 1 +exec airdefense2Server.cfg +set fs_savepath "./host"
